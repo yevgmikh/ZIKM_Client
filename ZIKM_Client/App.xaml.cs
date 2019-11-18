a54c0ec28db8fb9677d3f3322b5ead8cc7b65b43
yevgmikh <yevgeniy.mikhayluk@gmail.com>
2019-11-18 11:29:36 +0200

    Add project files.

---
+++ ZIKM_Client/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace ZIKM_Client
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
